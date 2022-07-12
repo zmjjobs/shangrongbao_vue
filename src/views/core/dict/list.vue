5c8f8759a51bf99b6ef1ad88c3e127533175e0b3
zmjjobs <dream98job@126.com>
2022-07-12 18:23:07 +0800

    first commit

---
+++ src/views/core/dict/list.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="app-container">
+    <div style="margin-bottom: 10px">
+      <el-button
+        @click="dialogVisible = true"
+        type="primary"
+        size="mini"
+        icon="el-icon-download"
+      >
+        导入Excel
+      </el-button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {}
+  },
+  methods: {},
+}
+</script>
